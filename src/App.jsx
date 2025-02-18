a04fa74a23c3a1d5a32a70afdd019a4b64b07a6a
farrux-torayev <farruxtorayev590@gmail.com>
2025-02-18 20:11:05 +0500

    fe

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Router from './Router';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import Router from "./Router";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <div >
-      <ToastContainer/>
-      <Router/>
+    <div>
+      <ToastContainer />
+      <Router />
     </div>
   );
-}
-
+};
+//dwedewdew
 export default App;
